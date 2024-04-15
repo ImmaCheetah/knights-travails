f5bcc821743c6388ddcc678078b98b9553176152
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-04-15 11:38:53 -0400

    Create basic move function that does BFS

--- index.mjs
+++ index.mjs
@@ -1,30 +1,5 @@
-/* 
-Define how a knight can move 
-Given a coordinate find all possible moves from that coordinate
-Visit one coordinate
-    - check if destination
-        - if it is, finish
-        - if not, add all possible moves from that coordinate to queue
-            - move to next coordinate  
-*/
 function Node(x = null, y = null, movesAvailable = []) {
     return { x, y, movesAvailable }
-}
-
-
-function chessBoard() {
-    let boardArray = [];
-
-    for (let i = 0; i < 8; i++) {
-
-    }
-}
-
-function moveLeftUp(node) {
-    node.x -= 1;
-    node.y += 2;
-
-    return node;
 }
 
 function possibleMoves(node) {
@@ -49,9 +24,42 @@ function possibleMoves(node) {
 }
 
 let testNode = Node(6, 7);
-console.log(possibleMoves(testNode));
-
 
 function knightMoves(start, end) {
+    let startNode = Node(start[0], start[1])
+    let queue = [];
+    
+    queue.push(startNode);
+    
+    while (queue.length != 0) {
+        let visitedNode = queue.shift();
 
+        if (visitedNode.x === end[0] && visitedNode.y === end[1]) {
+            return visitedNode + "reached!";
+        }   
+        
+        let allMovesOfNode = possibleMoves(visitedNode);
+        
+        allMovesOfNode.movesAvailable.forEach(element => {
+            queue.push(element);
+        });
+    }
+    
 }
+
+// console.log(possibleMoves(testNode));
+console.log(knightMoves([2, 3], [6, 3]));
+
+
+/* 
+Define how a knight can move 
+
+Given a coordinate find all possible moves from that coordinate
+Create a queue
+Add first node to queue
+Visit one coordinate
+    - check if destination
+        - if it is, finish
+        - if not, add all possible moves from that coordinate to queue
+            - move to next coordinate  
+*/
